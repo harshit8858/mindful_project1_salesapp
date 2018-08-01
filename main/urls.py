9b981307561666329a57789024c43a675d7f1e4c
Siddhant verma <harshit8858@gmail.com>
2018-08-01 11:05:32 +0530

    minor changes in urls

--- main/urls.py
+++ main/urls.py
@@ -4,12 +4,12 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', login, name='login'),
-    url(r'^home/$', index, name='index'),
-    url(r'^user/$', user, name='user'),
-    url(r'^signup/$', signup, name='signup'),
-    url(r'^auth_check/$', auth_check, name='check'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^invalid/$', invalid, name='invalid'),
-    url(r'^customer/$', customer, name='customer'),
-    url(r'^customer_list/$', customer_list, name='customer_list'),
+    url(r'^home/', index, name='index'),
+    url(r'^user/', user, name='user'),
+    url(r'^signup/', signup, name='signup'),
+    url(r'^auth_check/', auth_check, name='check'),
+    url(r'^logout/', logout, name='logout'),
+    url(r'^invalid/', invalid, name='invalid'),
+    url(r'^customer/', customer, name='customer'),
+    url(r'^customer_list/', customer_list, name='customer_list'),
 ]
