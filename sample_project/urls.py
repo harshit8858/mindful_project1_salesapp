bb13de8bf3ce0c646c30759e3ee78ea376459c24
Siddhant verma <harshit8858@gmail.com>
2018-08-01 11:01:48 +0530

    corrected urls format

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include(('main.urls', 'main'), namespace="main")),
-    url(r'^product/', include(('product.urls', 'product'), namespace="product")),
-    # url(r'^order/', include(('order.urls', 'order'), namespace="order"))
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', include(('main.urls', 'main'), namespace="main")),
+    url(r'^product/$', include(('product.urls', 'product'), namespace="product")),
+    # url(r'^order/$', include(('order.urls', 'order'), namespace="order"))
 ]
