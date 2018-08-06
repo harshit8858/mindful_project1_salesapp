4fbe9f206bcb9eac33e1f37f012d55b66b54a56a
harshit verma <harshit8858@gmail.com>
2018-08-07 01:35:36 +0530

    https://github.com/bmentges/django-cart removed

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    url(r'^add_to_cart/(\d+)/', add_to_cart, name='add_to_cart'),
-    url(r'^remove_to_cart/(?P<slug>[\w-]+)/', remove_from_cart, name='remove_to_cart'),
-    url(r'^cart/', get_cart, name='cart'),
 
 ]
