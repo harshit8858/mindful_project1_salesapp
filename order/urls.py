0fb469a32844bebe860cd62d15075118e7ba4c1b
harshit verma <harshit8858@gmail.com>
2018-08-07 22:53:03 +0530

    order complete

--- order/urls.py
+++ order/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     url(r'^new_order/', new_order, name='new_order'),
     url(r'^all_order/', all_order, name='all_order'),
-    url(r'^(\d+)/', order_details, name='order_details'),
+    url(r'^(\d+)/$', order_details, name='order_details'),
+    url(r'^(\d+)/edit/', order_edit, name='order_edit'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from.forms import *
 from .models import *
 
@@ -7,8 +8,10 @@ def new_order(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('order:new_order')
+            f = form.save()
+            f.save()
+            return redirect('order:order_details', f.id )
+            # return HttpResponseRedirect('')
     else:
         form = OrderForm()
     if request.user.is_superuser:
@@ -30,4 +33,24 @@ def order_details(request, order_id):
     context = {
         'instance': instance,
     }
-    return render(request, 'order/order_details.html', context)
+    if request.user.is_superuser:
+        return render(request, 'order/order_details.html', context)
+    else:
+        return render(request, 'main/not_authenticated.html')
+
+
+def order_edit(request, order_id):
+    instance = Order.objects.get(id=order_id)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=instance)
+        if form.is_valid():
+            f = form.save()
+            f.save()
+            return redirect('order:order_details', f.id )
+            # return HttpResponseRedirect('')
+    else:
+        form = OrderForm(instance=instance)
+    if request.user.is_superuser:
+        return render(request, 'order/new_order.html', {'form':form})
+    else:
+        return render(request, 'main/not_authenticated.html')
